fabd55109bada7b0accadb0474f2a2910d1e95a2
Jianbo Ye <jxy198@ist.psu.edu>
2015-12-23 19:47:45 -0500

    some changes

--- demo.jl
+++ demo.jl
@@ -10,34 +10,41 @@ p1=float(data(convert(Image{Gray}, load("$secondFrame.png"))));
 @show sum(p0)
 @show sum(p1)
 
+dir="";
+if length(ARGS) > 5
+  dir=string(ARGS[6], "/");
+end
+
 T=parse(ARGS[3]) # time ticks
 result=solveGeodesic(p0, p1, T, δ=float(ARGS[4])/pi);
 
-t=round((T+1)/2);
+t=round(Int, (T+1)/2);
 U=result[1];
 ζ = reshape(U.ζ[t,:],U.cdim[2:end]...);
 #imshow(ζ,cmap="bwr",vmin=-.05, vmax=.05);
-imsave("circle_$(firstFrame)",transpose(ζ),cmap="bwr",vmin=-.05, vmax=.05);
+imsave("$(dir)source_$(firstFrame)",transpose(ζ),cmap="bwr",vmin=-.05, vmax=.05);
 
 ## write interpolating frames
 #for i=1:(T+1)
 #    interp=min(1,reshape(result[1].ρ[i,:,:],size(p0)));
-#    imwrite(grayim(interp), "$firstFrame-$i.png")
+#    imwrite(grayim(interp), "$(dir)$firstFrame-$i.png")
 #end
 
 ## compute estimation error if the ground-truth frame is given
 if length(ARGS) > 4
   midFrame=ARGS[5];
-  mid=float(data(convert(Image{Gray}, load("$midFrame.png"))));
-  estmid=min(1, reshape(result[1].ρ[round(Int, T/2) + 1,:,:],size(p0)));
-  err_msqr = sqrt(mean((mid - estmid).^2));
-  err_mabs = mean(abs(mid-estmid))
-  @show err_msqr
-  @show err_mabs
+  if (midFrame != "-") 
+    mid=float(data(convert(Image{Gray}, load("$midFrame.png"))));
+    estmid=min(1, reshape(result[1].ρ[t,:,:],size(p0)));
+    err_msqr = sqrt(mean((mid - estmid).^2));
+    err_mabs = mean(abs(mid-estmid))
+    @show err_msqr
+    @show err_mabs
+  end
 end
 
 ## write out data
-matwrite("$firstFrame-data.mat", Dict{Any,Any}(
+matwrite("$(dir)$firstFrame-data.mat", Dict{Any,Any}(
            "mass" => U.ρ,
            "momentum" => U.ω,
            "source" => U.ζ))
@@ -50,7 +57,7 @@ height=size(p0)[2];
 ω2=result[1].ω[2][t,:,:];
 ω1=reshape(ω1, (width+1, height));
 ω2=reshape(ω2, (width, height+1));
-f=open("$firstFrame-flow.flo", "w");
+f=open("$(dir)$firstFrame-momentum.flo", "w");
 write(f, "PIEH");
 write(f, Int32(width));
 write(f, Int32(height));
